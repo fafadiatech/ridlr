5370928ab29a1120de35737a789a53da70dbfe6c
Sidharth Shah <sidharth@fafadiatech.com>
2020-03-09 21:33:55 +0530

    updated frontend skeleton

--- webapp/ridlr/frontend/ridlr/src/App.js
+++ webapp/ridlr/frontend/ridlr/src/App.js
@@ -1,10 +1,55 @@
-import React from 'react';
-import {Alert} from 'reactstrap';
+import React, { useState } from 'react';
+import {Alert, Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavbarText, Button, Progress, Container, Row, Col, Label, Input} from 'reactstrap';
+import Markdown from 'react-markdown';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <div className="App">
-      <Alert color="primary">This is cool!</Alert>
+      <Progress striped value="75" />
+      <Navbar color="light" light expand="md">
+              <NavbarBrand href="/">Ridlr</NavbarBrand>
+              <NavbarToggler onClick={toggle} />
+              <Collapse isOpen={isOpen} navbar>
+                <Nav className="mr-auto" navbar>
+                </Nav>
+                <NavbarText>Simple Text</NavbarText>&nbsp;&nbsp;
+                <Button color="primary"> Prev </Button>&nbsp;
+                <Button color="primary"> Next </Button>
+              </Collapse>
+        </Navbar>
+        <Alert color="primary">This is cool!</Alert>
+        <Container>
+          <Row>
+            <Markdown source="## Ut non tempor dolore sunt do cillum esse culpa dolor aute incididunt irure." /><br/><br/><br/>
+          </Row>
+          <Row>
+            <Col xs="6">
+              <Label check>
+                <Input type="radio" name="radio1" />{' '}Option one is this and that—be sure to include why it's great
+              </Label>
+            </Col>
+            <Col xs="6">
+              <Label check>
+                <Input type="radio" name="radio1" />{' '}Option one is this and that—be sure to include why it's great
+              </Label>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="6">
+              <Label check>
+                <Input type="radio" name="radio1" />{' '}Option one is this and that—be sure to include why it's great
+              </Label>
+            </Col>
+            <Col xs="6">
+              <Label check>
+                <Input type="radio" name="radio1" />{' '}Option one is this and that—be sure to include why it's great
+              </Label>
+            </Col>
+          </Row>
+        </Container>
     </div>
   );
 }
